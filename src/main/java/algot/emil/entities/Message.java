b54606cd4fd6f4e42f3cda8f415e02a79b788827
AlTheMan <algot98@hotmail.com>
2023-11-23 14:58:08 +0100

    Create Message.java

---
+++ src/main/java/algot/emil/entities/Message.java
@@ -0,0 +1,66 @@
+package algot.emil.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+
+@Table
+@Entity
+public class Message extends BaseEntity {
+
+
+    private LocalDate time;
+    private long employeeId;
+    private long patientId;
+
+    private long sentById; //from whom the message was sent from
+
+    //@ManyToOne
+    //private AppUser loggedInUser;
+    private String message;
+
+    public Message(long employeeId, long patientId, long sentById, String message) {
+        this.time = LocalDate.now();
+        this.employeeId = employeeId;
+        this.patientId = patientId;
+        this.sentById = sentById;
+        this.message = message;
+    }
+
+    public Message() {
+
+    }
+
+    public long getSentById() {
+        return sentById;
+    }
+
+    public LocalDate getTime() {
+        return time;
+    }
+
+    public long getEmployeeId() {
+        return employeeId;
+    }
+
+    public long getPatientId() {
+        return patientId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "time=" + time +
+                ", employeeId=" + employeeId +
+                ", patientId=" + patientId +
+                ", sentById=" + sentById +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
